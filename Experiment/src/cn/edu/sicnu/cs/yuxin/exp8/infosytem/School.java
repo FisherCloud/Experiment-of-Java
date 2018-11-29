61182b9735627598ca8d33c321b7fd360b26b9ed
Jenycc <903426943@qq.com>
2018-11-29 21:36:01 +0800

    update exp8

---
+++ Experiment/src/cn/edu/sicnu/cs/yuxin/exp8/infosytem/School.java
@@ -0,0 +1,40 @@
+package cn.edu.sicnu.cs.yuxin.exp8.infosytem;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class School {
+    private char[] identificationCode;
+    private String name = "";
+
+    public School() {
+        identificationCode = new char[10];
+        name = "";
+    }
+
+    public School(char[] identificationCode, String name) {
+        this.identificationCode = identificationCode;
+        this.name = name;
+    }
+
+    public void setIdentificationCode(char[] identificationCode) {
+        this.identificationCode = identificationCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char[] getIdentificationCode() {
+        return identificationCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "学校代码：" + Arrays.toString(identificationCode) + "，学校名称：" + name;
+    }
+}
---
+++ Experiment/src/cn/edu/sicnu/cs/yuxin/exp8/infosytem/mainFrame.java
@@ -0,0 +1,12 @@
+package cn.edu.sicnu.cs.yuxin.exp8.infosytem;
+
+import javax.swing.*;
+
+public class mainFrame extends JFrame {
+    private JPanel mainPanel;
+    private JTextField idCodeTextField;
+    private JTextField nameTextField;
+    private JButton submitButton;
+    private JLabel idCodeLabel;
+    private JLabel nameLabel;
+}
