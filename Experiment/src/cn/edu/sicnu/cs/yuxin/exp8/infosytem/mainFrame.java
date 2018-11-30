fab27aabc333c0f5e91e104ade7ea819ac98ef90
Jenycc <903426943@qq.com>
2018-11-30 10:50:08 +0800

    update exp8

--- Experiment/src/cn/edu/sicnu/cs/yuxin/exp8/infosytem/mainFrame.java
+++ Experiment/src/cn/edu/sicnu/cs/yuxin/exp8/infosytem/mainFrame.java
@@ -2,7 +2,16 @@ package cn.edu.sicnu.cs.yuxin.exp8.infosytem;
 
 import javax.swing.*;
 
-public class mainFrame extends JFrame {
+public class mainFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("mainFrame");
+        frame.setContentPane(new mainFrame().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(400, 320);
+    }
+
     private JPanel mainPanel;
     private JTextField idCodeTextField;
     private JTextField nameTextField;
