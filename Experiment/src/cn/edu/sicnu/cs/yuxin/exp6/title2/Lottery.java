4f5854daec6e9fe43c97b1d69fdb6069bba99239
Jenycc <903426943@qq.com>
2018-12-08 15:19:01 +0800

    update exp6,title2

--- Experiment/src/cn/edu/sicnu/cs/yuxin/exp6/title2/Lottery.java
+++ Experiment/src/cn/edu/sicnu/cs/yuxin/exp6/title2/Lottery.java
@@ -1,14 +1,11 @@
 package cn.edu.sicnu.cs.yuxin.exp6.title2;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Lottery {
     private int n;
     private int m;
     private Set<Integer> setN = new HashSet<Integer>();
-    private Set<Integer> setM = new HashSet<Integer>();
     private Set<Integer> setUserGuess = new HashSet<Integer>();
 
     public Lottery() {
@@ -17,10 +14,14 @@ public class Lottery {
     }
 
     public Lottery(int n, int m) {
-        if (n > 3 && n < 9) {
+        if (n >= 3 && n <= 9) {
             this.n = n;
             if (m <= n && m > 0) {
                 this.m = m;
+                Random random = new Random(System.currentTimeMillis());
+                while (setN.size() < n) {
+                    setN.add(random.nextInt(10));
+                }
             } else {
                 System.out.println("参数错误！");
             }
@@ -29,13 +30,43 @@ public class Lottery {
         }
     }
 
-    public boolean addGuess() {
-
-        return false;
+    public void addGuess() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < setN.size(); i++) {
+            System.out.println("请输入第" + (i + 1) + "个猜测的数字：");
+            if (!setUserGuess.add(scanner.nextInt())) {
+                System.out.println("数字重复，请重新输入：");
+                i--;
+            }
+        }
     }
 
-    public boolean getAward() {
+    public double getAward() {
+        System.out.println("随机产生的彩票号为：" + Arrays.toString(setN.toArray()));
+        System.out.println("您猜测的彩票号为：" + Arrays.toString(setUserGuess.toArray()));
+        if (setN.equals(setUserGuess)) {
+            return 100 * Math.pow(8, n);
+        } else if (setN.containsAll(setUserGuess)) {
+            return Math.pow(6, n);
+        } else {
+            int count = 0;
+            for (Integer integer : setUserGuess) {
+                if (setN.contains(integer)) {
+                    count++;
+                }
+            }
+            return count >= m ? 100 * Math.pow(2, count) : 0.0;
+        }
+    }
 
-        return false;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("输入彩票中需要的数字个数n（必须在3到9之间）：");
+        int n = input.nextInt();
+        System.out.println("输入彩票中需要匹配的最小数字数m（必须在1到n之间）:");
+        int m = input.nextInt();
+        Lottery lottery = new Lottery(n, m);
+        lottery.addGuess();
+        System.out.println("您获得的奖金为：" + lottery.getAward());
     }
 }
